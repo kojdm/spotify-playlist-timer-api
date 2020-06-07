6127cd33e697bd8c174164798351efd578ff2ff4
kojidelmundo <koji@tanda.co>
2020-06-07 21:51:54 +0800

    make api more RESTful

--- server.rb
+++ server.rb
@@ -26,22 +26,21 @@ MIN_DURATION = 900.freeze # 15 mins
 MAX_DURATION = 43200.freeze # 12 hours
 MAX_CATEGORIES = 5.freeze
 
-enable :sessions
-
 before do
   headers("Access-Control-Allow-Origin" => SPOTIFY_ACCT_URL)
-  init_session!
+  headers("Access-Control-Allow-Origin" => SPOTIFY_API_URL)
+  # init_session!
 end
 
-get "/" do
-  init_categories! if session_active?
+# get "/" do
+#   init_categories! if session_active?
 
-  erb :index, locals: {
-    current_user: @current_user,
-    categories: @categories || [],
-    base_url: request.base_url
-  }
-end
+#   erb :index, locals: {
+#     current_user: @current_user,
+#     categories: @categories || [],
+#     base_url: request.base_url
+#   }
+# end
 
 get "/login" do
   state = random_string(16)
@@ -59,62 +58,62 @@ get "/login" do
 end
 
 get "/callback" do
+  begone! unless (params.keys - ["code", "state"]).empty?
+
   code = params["code"]
   state = params["state"]
 
   res = SpotifyApi.post_token(code, state)
 
   access_token = res["access_token"]
-  refresh_token = res["refresh_token"]
+  _refresh_token = res["refresh_token"]
 
   api = SpotifyApi.new(access_token)
-  user = api.get("/me")
+  user = api.get("/me").to_h
+  categories = ApiObject.new(api: api).init_categories!(user["country"])
 
-  session[:user] = user.to_h
-  session[:access_token] = access_token
-  redirect("/")
+  {
+    user: user,
+    categories: categories.map(&:json_friendly),
+    access_token: access_token,
+  }.to_json
 end
 
 get "/generate_tracks" do
-  redirect("/") unless session_active?
+  access_token = request.env["HTTP_AUTHORIZATION"]
+  begone! unless (params.keys - ["seconds", "category_ids"]).empty? || access_token.nil?
 
+  access_token = access_token.split(" ").last
   desired_duration = params["seconds"].to_i
   category_ids = params["category_ids"].split(",")
 
-  redirect("/") unless desired_duration.between?(MIN_DURATION, MAX_DURATION) ||
+  begone! unless desired_duration.between?(MIN_DURATION, MAX_DURATION) ||
     category_ids.count.between?(1, MAX_CATEGORIES)
 
-  spoopi = Spoopi.new(desired_duration, category_ids, @api)
-  track_uris = spoopi.tracks.map(&:uri)
+  api = SpotifyApi.new(access_token)
+  spoopi = Spoopi.new(desired_duration, category_ids, api)
+  tracks = spoopi.tracks
 
-  p = {
-    :track_uris => track_uris.join(","),
-    :pl_name => params["playlist_name"],
-    :category_ids => category_ids,
-    :access_token => session[:access_token],
-    :current_user_id => @current_user["id"]
-  }
-
-  # redirect("/create_playlist?" + URI.encode_www_form(p))
-  HTTParty.post(
-    request.base_url + "/create_playlist",
-    body: p
-  )
+  {
+    tracks: tracks.map(&:json_friendly),
+    track_uris: tracks.map(&:uri).join(",")
+  }.to_json
 end
 
 post "/create_playlist" do
-  # redirect("/") unless session_active?
+  access_token = request.env["HTTP_AUTHORIZATION"]
+  begone! unless (params.keys - ["user_id", "track_uris", "pl_name", "category_ids"]).empty? || access_token.nil?
 
+  access_token = access_token.split(" ").last
+  user_id = params["user_id"]
   track_uris = params["track_uris"].split(",")
   pl_name = params["pl_name"]
   category_ids = params["category_ids"]
 
-  @api = SpotifyApi.new(params["access_token"])
-  current_user_id = params["current_user_id"]
+  api = SpotifyApi.new(access_token)
 
-  pl_obj = @api.post(
-    # "/users/#{@current_user["id"]}/playlists",
-    "/users/#{current_user_id}/playlists",
+  pl_obj = api.post(
+    "/users/#{user_id}/playlists",
     body: {
       name: pl_name,
       description: "Created with Spoopi - Spotify Playlist Timer. [cats: #{category_ids.join(", ")}]",
@@ -126,15 +125,15 @@ post "/create_playlist" do
     id: pl_obj["id"],
     name: pl_obj["name"],
     spotify_url: pl_obj["external_urls"]["spotify"],
-    api: @api
+    api: api
   )
 
   split_track_uris = track_uris.each_slice(100).to_a
   split_track_uris.each { |track_uris| new_playlist.add_tracks!(track_uris) }
 
-  body(json({playlist_url: new_playlist.spotify_url}))
-  status 200
-  # return "Playlist created! Start listening here: #{new_playlist.spotify_url}"
+  {
+    new_playlist: new_playlist.json_friendly
+  }.to_json
 end
 
 private
@@ -150,20 +149,25 @@ def random_string(length)
   text
 end
 
-def session_active?
-  !session[:user].nil? && !session[:access_token].nil?
+def begone!
+  return halt 400, "begone!"
 end
 
-def init_session!
-  return unless session_active?
+# def session_active?
+#   !session[:user].nil? && !session[:access_token].nil?
+# end
 
-  @current_user = session[:user]
-  @api = SpotifyApi.new(session[:access_token])
-end
+# def init_session!
+#   return unless session_active?
 
-def init_categories!
-  return unless session_active?
+#   @current_user = session[:user]
+#   @api = SpotifyApi.new(session[:access_token])
+# end
 
-  @categories = ApiObject.new(api: @api).init_categories!(@current_user["country"])
-end
+# def init_categories!
+#   return unless session_active?
+
+#   @categories = ApiObject.new(api: @api).init_categories!(@current_user["country"])
+# end
+
 

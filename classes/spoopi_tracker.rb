3c4c07371aaa8cf2a24a3fa1707736cf883f0ecc
kojidelmundo <koji@tanda.co>
2020-07-01 15:05:02 +0800

    remove unused methods

--- classes/spoopi_tracker.rb
+++ classes/spoopi_tracker.rb
@@ -23,46 +23,5 @@ class SpoopiTracker
       worksheet.insert_rows(worksheet.num_rows + 1, [[ date, category_ids, country_code, duration, track_count ]])
       worksheet.save
     end
-
-    def get_stats
-      stats_hash = {
-        spoopi_stats: {
-          total_playlists_created: 0,
-          total_duration_of_playlists: 0,
-          total_tracks_in_playlists: 0,
-          country_stats: {},
-          category_stats: {}
-        }
-      }
-      CSV.foreach("spoopi_stats.csv") do |row|
-        stats_hash[:spoopi_stats][:total_playlists_created] += 1
-        stats_hash[:spoopi_stats][:total_duration_of_playlists] += row[2].to_i
-        stats_hash[:spoopi_stats][:total_tracks_in_playlists] += row[3].to_i
-
-        country_count = stats_hash[:spoopi_stats][:country_stats][row[1]] || 0
-        stats_hash[:spoopi_stats][:country_stats][row[1]] = country_count + 1
-
-        categories = row[0].split("|")
-        categories.each do |cat|
-          category_count = stats_hash[:spoopi_stats][:category_stats][cat] || 0
-          stats_hash[:spoopi_stats][:category_stats][cat] = category_count + 1
-        end
-      end
-
-      stats_hash
-    end
-
-    private
-
-    def random_string(length)
-      text = ""
-      chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-
-      length.times do |i|
-        text += chars[rand(chars.length)]
-      end
-
-      text
-    end
   end
 end
